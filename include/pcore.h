5d3c4e6d2a08c6bee7e24a6da847b1f4be4fb249
Abhijeet Tripathy <abhijeetofficial1999@gmail.com>
2024-01-19 14:22:17 +0530

    pcore initial commit

---
+++ include/pcore.h
@@ -0,0 +1,14 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+class Pcore
+{
+private:
+    
+public:
+    Pcore();
+    ~Pcore();
+};
